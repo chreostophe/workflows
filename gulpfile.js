f47237d7b443f74b77a979560b17f761ed2124b6
C M <chreostophe@gmail.com>
2017-11-09 17:17:18 -0800

    Added Coffee Script Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
-    gutil = require('gulp-util');
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+
+
+var coffeesources = ['components/coffee/*.coffee'];
+
+
+gulp.task('coffee', function(){
+    gulp.src(coffeesources)
+        .pipe(coffee({ bare: true })
+          .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
